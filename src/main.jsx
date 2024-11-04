bd359bb3c792d69d912b7baf59056b2e8041aab3
EdbertoSantos <edbertosantos62@gmail.com>
2024-11-04 14:36:34 -0300

    Lançamento do repertorio para deploy

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import LoginClient from './routes/LoginClient.jsx';
+import NotFound from './routes/NotFound.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/SAP_LucasAlbertim",
+        element: <LoginClient />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
